585f89cdd0d2edfb583100f65ba4db5b439506bb
assiakheddia <assia.kheddia@icloud.com>
2024-05-17 00:15:46 +0100

    commit

--- orthophonisteApp/src/Controllers/Main.java
+++ orthophonisteApp/src/Controllers/Main.java
@@ -8,19 +8,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    private static Stage stg;
+    public static Stage stg;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         stg = primaryStage;
         primaryStage.setResizable(false);
-        Parent root = FXMLLoader.load(getClass().getResource("Layouts/SignUp.fxml"));
-
+    
+        // Update the path to correctly locate SignUp.fxml
+        Parent root = FXMLLoader.load(getClass().getResource("/Layouts/FicheDeSuivi/FicheDeSuivi.fxml"));
+    
         primaryStage.setTitle("SIGN UP");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 1100, 600));
         primaryStage.show();
-
     }
+    
     public void changeScene(String fxml) throws IOException {
         Parent pane = FXMLLoader.load(getClass().getResource(fxml));
         stg.setScene(new Scene(pane, 1100, 600));
