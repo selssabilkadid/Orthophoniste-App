2dfb3381549dc4bc5c9f796160b31988fb08c673
selssabil <selssabilkadid@gmail.com>
2024-05-19 20:09:43 +0200

    ui update

---
+++ orthophonisteApp/src/Layouts/MyTestsController.java
@@ -0,0 +1,11 @@
+package Layouts;
+
+import javafx.event.ActionEvent;
+
+public class MyTestsController {
+    public void Tests_switchform(ActionEvent actionEvent) {
+    }
+
+    public void Create_Question(ActionEvent actionEvent) {
+    }
+}
