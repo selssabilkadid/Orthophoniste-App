b31775b9b36d2754f1b63303d2cfd8c04d61f678
assiakheddia <assia.kheddia@icloud.com>
2024-05-26 23:15:07 +0100

    stuff

--- orthophonisteApp/src/Classes/Trouble.java
+++ orthophonisteApp/src/Classes/Trouble.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class Trouble  implements Serializable {
     private static final long serialVersionUID = 1L;
     private String nom;
-    private CategorieTrouble categorie;
+    public CategorieTrouble categorie;
 
     public Trouble(String nom, CategorieTrouble categorie){
         this.nom = nom;
